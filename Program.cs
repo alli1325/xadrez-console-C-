462d8aee147589248773e5965df378e2ecb97afb
Alli1325 <alli.moreira@gmail.com>
2021-01-20 13:57:15 -0300

    Aprimorando metodo imprimirTabuleiro

--- Program.cs
+++ Program.cs
@@ -15,6 +15,8 @@ namespace xadrez_console
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(3, 5));
+
                 Tela.imprimirTabuleiro(tab);
 
                 Console.WriteLine();
