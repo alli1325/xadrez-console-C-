257da170910e18ef15708d2578a5f25ecabb3cd8
Alli1325 <alli.moreira@gmail.com>
2021-01-19 23:40:38 -0300

    Tratando erros com excecao personalizada

---
+++ tabuleiro/TabuleiroException.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tabuleiro
+{
+    public class TabuleiroException : Exception
+    {
+        public TabuleiroException(String msg) : base(msg){
+            
+        }
+    }
+}
